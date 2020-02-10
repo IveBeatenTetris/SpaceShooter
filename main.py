943b94ab3ac408a76cb926540589318bdced0276
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2020-02-10 14:04:14 +0100

    capsulated game loop into a class-method

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame as pg
 
-pg .init()
-screen = pg.display.set_mode((700, 500))
-pg.display.set_caption("Space Shooter")
-clock = pg.time.Clock()
-running = True
+class Main(object):
+    """main class. will be called on execute."""
+    def __init__(self):
+        """."""
+        pg.init()
+        self.screen = pg.display.set_mode((700, 500))
+        pg.display.set_caption("Space Shooter")
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.loop()
+    def loop(self):
+        """pygame main loop."""
+        while self.running:
+            # events
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+            # updating
+            pg.display.update()
+            self.clock.tick(60)
 
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
-    pg.display.update()
-    clock.tick(60)
-
-pg.quit()
+if __name__ == '__main__':
+    Main()
